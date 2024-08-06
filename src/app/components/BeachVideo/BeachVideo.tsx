2bd4c8595cc3c670cc1701ff27d73aed9609016f
juanmagp80 <juangpdev@gmail.com>
2024-08-07 00:12:22 +0200

    continuo con slider

--- src/app/components/BeachVideo/BeachVideo.tsx
+++ src/app/components/BeachVideo/BeachVideo.tsx
@@ -3,6 +3,7 @@
 const BeachVideo = () => {
   return (
     <div className="relative w-full h-[800px] overflow-hidden">
+      {/* Video */}
       <div className="absolute inset-0 overflow-hidden">
         <video
           autoPlay
@@ -16,10 +17,20 @@ const BeachVideo = () => {
         </video>
       </div>
 
+      {/* Contenedor con efecto de espejo */}
+      <div className="absolute inset-0 flex items-center justify-center p-4">
+        <div className="bg-white bg-opacity-20 p-6 rounded-lg shadow-lg backdrop-blur-md">
+          <h1 className="text-5xl font-bold text-green-50 mb-4">FARMACIA SANTA MARIA</h1>
+          <p className="text-2xl text-center text-beige-300">Tu farmacia de confianza en La Cala del Moral.</p>
+        </div>
+      </div>
+
+      {/* Fondo estilizado en la parte inferior */}
       <div
         className="absolute bottom-0 left-0 w-full h-[150px] bg-white"
         style={{ clipPath: 'polygon(0 100%, 100% 0, 100% 100%, 0 100%)' }}
-      ></div>
+      >
+      </div>
     </div>
   );
 };
--- src/app/components/PromotionSlider/PromotionSlider.tsx
+++ src/app/components/PromotionSlider/PromotionSlider.tsx
@@ -1,45 +1,38 @@
 "use client";
 
-import useEmblaCarousel from 'embla-carousel-react';
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 
 const PromotionSlider: React.FC = () => {
-    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
-    const autoplayRef = useRef<NodeJS.Timeout | null>(null);
-
-    useEffect(() => {
-        if (emblaApi) {
-            const autoplay = () => {
-                autoplayRef.current = setInterval(() => {
-                    if (emblaApi) {
-                        emblaApi.scrollNext();
-                    }
-                }, 3000); // Cambia de diapositiva cada 3 segundos
-            };
-
-            autoplay();
-
-            return () => {
-                if (autoplayRef.current) {
-                    clearInterval(autoplayRef.current);
-                }
-            };
-        }
-    }, [emblaApi]);
+    const settings = {
+        dots: true, // Muestra los puntos de navegación
+        infinite: true, // Habilita el desplazamiento infinito
+        speed: 400, // Velocidad del desplazamiento
+        slidesToShow: 1, // Muestra una diapositiva a la vez
+        slidesToScroll: 1, // Desplaza una diapositiva a la vez
+        autoplay: true, // Habilita el autoplay
+        autoplaySpeed: 3000, // Tiempo entre desplazamientos en ms
+    };
 
     return (
-        <div className="embla">
-            <div className="embla__viewport" ref={emblaRef}>
-                <div className="embla__container">
-                    <div className="embla__slide">
-                        <img src="/promociones1.jpg" alt="Promoción 1" />
-                    </div>
-                    <div className="embla__slide">
-                        <img src="/promociones2.png" alt="Promoción 2" />
-                    </div>
-                    {/* Añade más diapositivas aquí */}
+        <div className="w-4/5 max-w-4xl mx-auto bg-cover bg-center rounded-xl p-5" >
+            <Slider {...settings}>
+                <div>
+                    <img src="/promociones1.jpg" alt="Promoción 1" className="w-full h-full object-cover rounded-xl" />
                 </div>
-            </div>
+                <div>
+                    <img src="/promociones2.png" alt="Promoción 2" className="w-full h-full object-cover rounded-xl" />
+                </div>
+                <div>
+                    <img src="/promociones3.jpg" alt="Promoción 1" className="w-full h-full object-cover rounded-xl" />
+                </div>
+                <div>
+                    <img src="/promociones4.jpg" alt="Promoción 1" className="w-full h-full object-cover rounded-xl" />
+                </div>
+                {/* Añade más diapositivas aquí */}
+            </Slider>
         </div>
     );
 };
